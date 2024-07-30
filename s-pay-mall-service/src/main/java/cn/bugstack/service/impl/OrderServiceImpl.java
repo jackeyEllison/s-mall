c99111192e06ef69babd82441a8b3d4604f3d8fe
小傅哥 <184172133@qq.com>
2024-07-31 07:36:45 +0800

    feat: mvc 改造 - 下单服务

--- s-pay-mall-service/src/main/java/cn/bugstack/service/impl/OrderServiceImpl.java
+++ s-pay-mall-service/src/main/java/cn/bugstack/service/impl/OrderServiceImpl.java
@@ -5,16 +5,36 @@ import cn.bugstack.dao.IOrderDao;
 import cn.bugstack.domain.po.PayOrder;
 import cn.bugstack.domain.req.ShopCartReq;
 import cn.bugstack.domain.res.PayOrderRes;
+import cn.bugstack.domain.vo.ProductVO;
 import cn.bugstack.service.IOrderService;
+import cn.bugstack.service.rpc.ProductRPC;
+import com.alibaba.fastjson.JSONObject;
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.AlipayClient;
+import com.alipay.api.request.AlipayTradePagePayRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
+import java.util.Date;
 
+@Slf4j
 @Service
 public class OrderServiceImpl implements IOrderService {
 
+    @Value("${alipay.notify_url}")
+    private String notifyUrl;
+    @Value("${alipay.return_url}")
+    private String returnUrl;
     @Resource
     private IOrderDao orderDao;
+    @Resource
+    private ProductRPC productRPC;
+    @Resource
+    private AlipayClient alipayClient;
 
     @Override
     public PayOrderRes createOrder(ShopCartReq shopCartReq) throws Exception {
@@ -24,12 +44,66 @@ public class OrderServiceImpl implements IOrderService {
         payOrderReq.setProductId(shopCartReq.getProductId());
         PayOrder unpaidOrder = orderDao.queryUnPayOrder(payOrderReq);
 
-        if (null != unpaidOrder && Constants.OrderStatusEnum.PAY_WAIT.equals(unpaidOrder.getStatus())) {
-
+        if (null != unpaidOrder && Constants.OrderStatusEnum.PAY_WAIT.getCode().equals(unpaidOrder.getStatus())) {
+            log.info("创建订单-存在，已存在未支付订单。userId:{} productId:{} orderId:{}", shopCartReq.getUserId(), shopCartReq.getProductId(), unpaidOrder.getOrderId());
+            return PayOrderRes.builder()
+                    .orderId(unpaidOrder.getOrderId())
+                    .payUrl(unpaidOrder.getPayUrl())
+                    .build();
+        } else if (null != unpaidOrder && Constants.OrderStatusEnum.CREATE.getCode().equals(unpaidOrder.getStatus())) {
+            log.info("创建订单-存在，存在未创建支付单订单，创建支付单开始 userId:{} productId:{} orderId:{}", shopCartReq.getUserId(), shopCartReq.getProductId(), unpaidOrder.getOrderId());
+            PayOrder payOrder = this.doPrepayOrder(shopCartReq.getUserId(), shopCartReq.getProductId(), unpaidOrder.getProductName(), unpaidOrder.getOrderId(), unpaidOrder.getTotalAmount());
+            return PayOrderRes.builder()
+                    .orderId(payOrder.getOrderId())
+                    .payUrl(payOrder.getPayUrl())
+                    .build();
         }
 
+        // 2. 查询商品 & 创建订单
+        ProductVO productVO = productRPC.queryProductByProductId(shopCartReq.getProductId());
+        String orderId = RandomStringUtils.randomNumeric(16);
+        orderDao.insert(PayOrder.builder()
+                .userId(shopCartReq.getUserId())
+                .productId(productVO.getProductId())
+                .productName(productVO.getProductName())
+                .orderId(orderId)
+                .orderTime(new Date())
+                .totalAmount(productVO.getPrice())
+                .status(Constants.OrderStatusEnum.CREATE.getCode())
+                .build());
 
-        return null;
+        // 3. 创建支付单
+        PayOrder payOrder = doPrepayOrder(shopCartReq.getUserId(), shopCartReq.getProductId(), productVO.getProductName(), orderId, productVO.getPrice());
+
+        return PayOrderRes.builder()
+                .orderId(orderId)
+                .payUrl(payOrder.getPayUrl())
+                .build();
+    }
+
+    private PayOrder doPrepayOrder(String userId, String productId, String productName, String orderId, BigDecimal totalAmount) throws AlipayApiException {
+        AlipayTradePagePayRequest request = new AlipayTradePagePayRequest();
+        request.setNotifyUrl(notifyUrl);
+        request.setReturnUrl(returnUrl);
+
+        JSONObject bizContent = new JSONObject();
+        bizContent.put("out_trade_no", orderId);
+        bizContent.put("total_amount", totalAmount.toString());
+        bizContent.put("subject", productName);
+        bizContent.put("product_code", "FAST_INSTANT_TRADE_PAY");
+        request.setBizContent(bizContent.toString());
+
+        String form = alipayClient.pageExecute(request).getBody();
+
+        PayOrder payOrder = new PayOrder();
+        payOrder.setOrderId(orderId);
+        payOrder.setPayUrl(form);
+        payOrder.setStatus(Constants.OrderStatusEnum.PAY_WAIT.getCode());
+
+        // 更新订单支付信息
+        orderDao.updateOrderPayInfo(payOrder);
+
+        return payOrder;
     }
 
 }
