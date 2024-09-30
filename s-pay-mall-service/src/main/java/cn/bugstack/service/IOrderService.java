66c84b14bf3968d3b2ba9c1d4939c1f5e17030f9
fuzhengwei <184172133@qq.com>
2024-09-30 10:57:33 +0800

    feat: 《小型支付商城系统》第3-6节：支付回调处理

--- s-pay-mall-service/src/main/java/cn/bugstack/service/IOrderService.java
+++ s-pay-mall-service/src/main/java/cn/bugstack/service/IOrderService.java
@@ -3,6 +3,8 @@ package cn.bugstack.service;
 import cn.bugstack.domain.req.ShopCartReq;
 import cn.bugstack.domain.res.PayOrderRes;
 
+import java.util.List;
+
 /**
  * @author Fuzhengwei bugstack.cn @小傅哥
  * @description 订单服务接口
@@ -11,5 +13,13 @@ import cn.bugstack.domain.res.PayOrderRes;
 public interface IOrderService {
 
     PayOrderRes createOrder(ShopCartReq shopCartReq) throws Exception;
+
+    void changeOrderPaySuccess(String orderId);
+
+    List<String> queryNoPayNotifyOrder();
+
+    List<String> queryTimeoutCloseOrderList();
+
+    boolean changeOrderClose(String orderId);
 
 }
